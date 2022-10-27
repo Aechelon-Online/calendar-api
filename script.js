f7f64d07196b0823edeffca445d0a63e1dd76f5a
Aechelon-Online <info@aechelon.online>
2022-10-27 11:37:36 -0500

    Fixed anchor tags

--- script.js
+++ script.js
@@ -20,26 +20,24 @@
       const summary = data["items"][i]['summary']
       const location = data['items'][i]['location']
       const locationParts = location.split(",")
-      const venue = locationParts[0]
-      // const street = locationParts[1]
       const city = locationParts[2]
       const stateAndZip = locationParts[3]
       const stateSplit = stateAndZip.split(" ")
       const state = stateSplit[1]
 
+      console.log(locationParts[1])
+      console.log(locationParts[2])
+      console.log(locationParts[3])
+
 
       const dateTime = data['items'][i]['start']['dateTime']
-      const realDate = moment(dateTime).format('LL')
+      const realDate = moment(dateTime).format('ll')
       const realTime = moment(dateTime).format('LT')
       const dateSplit = realDate.split(",",1).toString()
       const monthDateSplit = dateSplit.split(" ")
       const month = monthDateSplit[0]
       const date = monthDateSplit[1]
-  
-      console.log(stateAndZip)
-      console.log(stateSplit)
-      console.log(state)
-
+      
       
       document.querySelector(".calendar-api").innerHTML += 
       `
@@ -49,7 +47,7 @@
           <h1 class="day">${date}</h1>
         </div>
         <div class="date-loc">
-          <h1 class="location">${summary}</h1>
+          <h1 class="location"><a class="google-map" target="_blank" href="https://www.google.com/maps/search/?api=1&query=${location}">${summary}</a></h1>
           <h1 class="location">${city + " " + state}</h1>
           <h1 class="dateTime">${realTime}</h1>
         </div>
@@ -58,3 +56,5 @@
       }
 }
 getData()
+
+
